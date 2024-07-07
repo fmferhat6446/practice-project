481b0fc1a34de86216417af4d322081c86a93066
fmferhat6446 <mhmdforhad333@gmail.com>
2024-07-07 18:41:54 +0600

    fizzbuzz practice home

--- fizz.js
+++ fizz.js
@@ -1,24 +1,50 @@
 
 
 
-const getFizzBizz = ( ) => {
-    for (let i = 1; i <=100; i++) {
-        if (i%3===0 && i%5===0) {
-            console.log("FizzBuzz");
-        }
+// const getFizzBizz = ( ) => {
+//     for (let i = 1; i <=100; i++) {
+//         if (i%3===0 && i%5===0) {
+//             console.log("FizzBuzz");
+//         }
         
-        else if (i%3===0) {
-            console.log("Fizz");
-        }
+//         else if (i%3===0) {
+//             console.log("Fizz");
+//         }
 
-        else if (i%5===0) {
-            console.log("Buzz");
-        }
+//         else if (i%5===0) {
+//             console.log("Buzz");
+//         }
 
-        else {
-            console.log(i);
-        }
+//         else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// getFizzBizz()
+
+
+const FizzBuzz = function fizzBuzz() {
+    
+    for (let i = 1; i <=100; i++) {
+       if (i%3===0 && i%5===0) {
+        console.log("fizzBuzz");
+       }
+
+       else if (i%3===0) {
+        console.log(fizz);
+       }
+
+       else if (i%5===0) {
+        console.log(buzz);
+       }
+        
+       else{
+        console.log(i);
+       }
     }
 }
 
-getFizzBizz()
+FizzBuzz();
+
+
